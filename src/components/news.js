96dab741830566eb04faaa45b566e8ba46e5553a
unknown <jessedraper2@gmail.com>
2022-03-26 17:56:43 -0600

    wip

---
+++ src/components/news.js
@@ -0,0 +1,17 @@
+
+
+const News = () => {
+    
+    return(
+        <>
+        <div>
+            <img></img>
+            <div>
+                <h1>New Pokemon Games!</h1>
+                <p> Pokemon has annonced the new additions to the mainline series <strong>Pokemon Scarlet</strong> and <strong>Pokemon Violet</strong>. These are anticipated to launch at the end of 2022 (probably November to December)</p>
+            </div>
+        </div>
+        </>
+    )
+}
+export default News;
