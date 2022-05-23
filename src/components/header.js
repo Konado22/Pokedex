173654ca5a3b720d611019428076c6984ed6f501
unknown <jessedraper2@gmail.com>
2022-05-23 10:56:13 -0600

    redo search

--- src/components/header.js
+++ src/components/header.js
@@ -7,14 +7,15 @@ const Header = () => {
         <div style={{"display":"flex", "justifyContent" : "center", "flexDirection": "column"}}>
             {/* <img src={pokeHeader} ></img> */}
             {/* needs media querries or formatting from container in react-bootstrap */}
-            {/* <img src={pokeBall}></img> */}
             <img></img>
         </div>
         <div style={{"paddingRight": "10px", "backgroundColor": "blue", "color": "white", "display": "flex", "flexDirection": "row", "justifyContent":"space-evenly", "alignItems": "flex-end"}}>
-                <h1>Home</h1>
-                <h1> Search Pokemon</h1>
-                <h1>Favorites</h1>
-                <h1>Logout</h1>
+        <img src={pokeBall} style={{height:"100px", width:"100px"}}></img>
+
+                {/* <h1>Home</h1>
+                <h1> Search Pokemon</h1> */}
+                {/* <h1>Favorites</h1>
+                <h1>Logout</h1> */}
         </div>
         </>
     )
--- src/components/pokeSearch.js
+++ src/components/pokeSearch.js
@@ -1,25 +1,25 @@
-import { useState, useEffect } from "react"
-import { Form } from "react-bootstrap"
-import axios from "axios"
-const PokeSearch = () => { 
-    function searchThis () {
-        fetch("").then((response) => {
-            response.json
-        }).then((data)=> {
-            return data
-        })
-    }
-    const [pokemon, setPokemon]= useState()
-    return (
-        <Form>
-            <Form.Group>
-                <Form.Label>
-                    Use this form to search for any pokemon 
-                </Form.Label>
-                <Form.Control type="searchbar" placeholder="type here" className="searchbar">
-                </Form.Control>
-            </Form.Group>  
-        </Form>
-    )
-    }
-export default PokeSearch;
+import {useEffect, useState} from 'react'
+export default function PokeSearch () {
+const [arr, setArr] = useState()
+useEffect(() => {})
+let array = []
+function Search () {fetch(" https://pokeapi.co/api/v2/pokemon/ditto").then( (data) =>{data.json()}).then((res) => {
+    array.push(res)
+    console.log(res)
+    array.map((pokemon) => {
+        return (
+            <div>
+                <h1>{pokemon.name}</h1>
+                <img src={pokemon.image}></img>
+                <h2>{pokemon.type}</h2>
+            </div>
+        )
+    })
+}).catch(err =>  {console.log(err)})}
+return(
+    <>
+    <h1>Click to search pokemon</h1>
+    <button onClick={()=>{Search()}}>Search</button>
+    </>
+)
+}
